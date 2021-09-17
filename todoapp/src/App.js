4a1e65c02935e1deb30f58cac8dcd0058f2a3fcc
Rakesh60 <rakeshgupta5353@gmail.com>
2021-09-17 23:05:53 +0530

    basics of react

---
+++ todoapp/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "./Button";
+const App = () => {
+  return (
+    <div>
+      <h1 className="h1">Hello World </h1>
+      <h2>Hello Users</h2>
+      <Button buttonText="Apple" />
+      <Button buttonText="Gauva" />
+      <Button buttonText="Orange" />
+    </div>
+  );
+};
+
+const Appy = () => {};
+
+export default App;
